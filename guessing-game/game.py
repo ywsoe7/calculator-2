11461ca0d7c2fc7f082c66da4e1f6117b2a54d75
Yoon Soe <yoonsoe25@gmail.com>
2022-02-08 05:43:56 +0000

    removed comments

--- guessing-game/game.py
+++ guessing-game/game.py
@@ -1,21 +1,5 @@
 import random
 
-# A number-guessing game.
-# greet user
-# gave user input name
-# choose random number between 1-100
-# repeat the loop
-     #input user's guess
-     #if guess is not correct:
-         #give hint
-            #if not a number, re state same range
-            #changes range, and give new range
-         #increase user guess count by 1
-    #else:
-         #good job!
-
-# Put your code here
-
 print('Hi! Welcome! What is your name?')
 name = input('Type in your name: ')
 
@@ -32,10 +16,9 @@ low = 0
 high = 101
 count = 1
 
-
 while your_guess != random_number:
     count += 1
-
+        
     if your_guess > random_number:
         high = your_guess
         try:
@@ -53,22 +36,6 @@ while your_guess != random_number:
 
 if your_guess == random_number:
     if count == 1:
-        print(f"WOW!!! You guessed it in just ONE try! {name}, go treat yourself to some icecream!!")
+        print(f"WOW!!! You guessed it in just ONE try! {name}, go treat yourself to some icecream!")
     else:
-        print(f"Congratulations, {name}! You have guessed the correct answer in {count} tries.") #ADD COUNT
-
-
-# The try block lets you test a block of code for errors.
-
-# The except block lets you handle the error.
-
-# The else block lets you execute code when there is no error.
-
-# The finally block lets you execute code, regardless of the result of the try- and except blocks.
-
-# try:
-#   print(x)
-# except NameError:
-#   print("Variable x is not defined")
-# except:
-#   print("Something else went wrong")
+        print(f"Congratulations, {name}! You have guessed the correct answer in {count} tries.")
